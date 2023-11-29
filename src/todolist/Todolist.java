fcd809ffba25988caec98855be509b0ccccf0833
Prasathappy <132459112+Prasathappy@users.noreply.github.com>
2023-11-29 18:33:05 +0530

    Update Todolist.java

--- src/todolist/Todolist.java
+++ src/todolist/Todolist.java
@@ -3,11 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package todolist;
-
-/**
- *
- * @author SATHYARAM_P_CIT
- */
 public class Todolist {
 
     public static void main(String[] args) {
